1f115ce49804a5f150cb420e206167b22ff77a30
Ivan Savchuk <116903781+IvanSavchuk7@users.noreply.github.com>
2025-06-16 17:26:53 +0300

    Add files via upload

---
+++ apply/index.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("form");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const submitBtn = document.getElementById("submit-btn");
+        submitBtn.disabled = true; //
+        const loader = submitBtn.querySelector(".dot-spinner");
+        const text = submitBtn.querySelector("span");
+        const arrowOuter = submitBtn.querySelector(".arrow");
+        arrowOuter.style.display = "none";
+        text.innerText = "";
+        submitBtn.childNodes[submitBtn.childNodes.length - 1].textContent = "";
+        loader.style.display = "block";
+
+        const formData = new FormData(form);
+
+        fetch("https://script.google.com/macros/s/AKfycbzogLbouUG0_SAFx7WLzi0ulY4B68W-Pvs8Q41eZJ-JYowyzcqUvdXm2ek7pEUmtd4-/exec", {
+            method: "POST",
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.result === "success") {
+                    const submitBtn = document.getElementById("submit-btn");
+                    submitBtn.style.display = "flex";
+                    submitBtn.style.alignItems = "center";
+                    submitBtn.style.justifyContent = "center";
+                    arrowOuter.style.display = "block";
+                    arrowOuter.style.background = "none";
+
+                    arrowOuter.style.display = "block";
+                    loader.style.display = "none";
+                    arrowOuter.src = "../assets/images/checkmark.svg"; // ✅ Your checkmark icon
+                    arrowOuter.style.background = "none";
+                    submitBtn.classList.add("submitted");
+                }
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                alert("Something went wrong. Please try again later.");
+            });
+    });
+});
